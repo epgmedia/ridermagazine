163c9627fa2e4174b51b1719df881a8f23b38f9a
Christopher Gerber <chriswgerber@gmail.com>
2015-01-09 13:02:01 -0600

    Ads now managed in seperate file. Preparing for plugin.

---
+++ wp-content/themes/gopublish/inc/ads/class.dfp_ad_positions.php
@@ -0,0 +1,159 @@
+<?php
+
+Class DFP_Ad_Positions {
+
+	public $div_id = 'div-gpt-ad-1375711686389';
+
+	protected $account_id = '/35190362/';
+
+	public $ad_positions = array(
+		array(
+			'ad_name'      => 'RDR_ROS_160_SB1',
+			'position_tag' => 'div-gpt-ad-1375711686389-0',
+			'sizes'        => [
+				[ 120, 240 ],
+				[ 125, 125 ],
+				[ 120, 600 ],
+				[ 160, 160 ],
+				[ 160, 240 ],
+				[ 160, 600 ]
+			],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_160_SB2',
+			'position_tag' => 'div-gpt-ad-1375711686389-1',
+			'sizes'        => [
+				[ 120, 240 ],
+				[ 125, 125 ],
+				[ 120, 600 ],
+				[ 160, 160 ],
+				[ 160, 240 ],
+				[ 160, 600 ]
+			],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_160_SB3',
+			'position_tag' => 'div-gpt-ad-1375711686389-2',
+			'sizes'        => [
+				[ 120, 240 ],
+				[ 125, 125 ],
+				[ 120, 600 ],
+				[ 160, 160 ],
+				[ 160, 240 ],
+				[ 160, 600 ]
+			],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_160_SB4',
+			'position_tag' => 'div-gpt-ad-1375711686389-3',
+			'sizes'        => [
+				[ 120, 240 ],
+				[ 125, 125 ],
+				[ 120, 600 ],
+				[ 160, 160 ],
+				[ 160, 240 ],
+				[ 160, 600 ]
+			],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_160_SB5',
+			'position_tag' => 'div-gpt-ad-1375711686389-4',
+			'sizes'        => [
+				[ 120, 240 ],
+				[ 125, 125 ],
+				[ 120, 600 ],
+				[ 160, 160 ],
+				[ 160, 240 ],
+				[ 160, 600 ]
+			],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_160_SB6',
+			'position_tag' => 'div-gpt-ad-1375711686389-5',
+			'sizes'        => [
+				[ 120, 240 ],
+				[ 125, 125 ],
+				[ 120, 600 ],
+				[ 160, 160 ],
+				[ 160, 240 ],
+				[ 160, 600 ]
+			],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_160_SB7',
+			'position_tag' => 'div-gpt-ad-1375711686389-6',
+			'sizes'        => [
+				[ 120, 240 ],
+				[ 125, 125 ],
+				[ 120, 600 ],
+				[ 160, 160 ],
+				[ 160, 240 ],
+				[ 160, 600 ]
+			],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_300_LR',
+			'position_tag' => 'div-gpt-ad-1375711686389-7',
+			'sizes'        => [ 300, 250 ],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_300_Mid',
+			'position_tag' => 'div-gpt-ad-1375711686389-8',
+			'sizes'        => [ 300, 250 ],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_300_Mid2',
+			'position_tag' => 'div-gpt-ad-1375711686389-9',
+			'sizes'        => [ 300, 250 ],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_300_UR',
+			'position_tag' => 'div-gpt-ad-1398710304449-0',
+			'sizes'        => [ [ 300, 250 ], [ 300, 600 ] ],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_middle468',
+			'position_tag' => 'div-gpt-ad-1375711686389-13',
+			'sizes'        => [ 468, 60 ],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_middle468_2',
+			'position_tag' => 'div-gpt-ad-1375711686389-14',
+			'sizes'        => [ 468, 60 ],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_Footerboard',
+			'position_tag' => 'div-gpt-ad-1375711686389-11',
+			'sizes'        => [ [ 468, 60 ], [ 728, 90 ] ],
+			'out_of_page'  => FALSE
+		),
+		array(
+			'ad_name'      => 'RDR_ROS_Leaderboard',
+			'position_tag' => 'div-gpt-ad-1375711686389-12',
+			'sizes'        => [ 728, 90 ],
+			'out_of_page'  => FALSE
+		)
+	);
+
+	public function get_positions() {
+
+		/* Get Ad Positions and add acct it and div id */
+
+
+
+	}
+
+}
